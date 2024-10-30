e88460d7a8698702a830de477d76fd2af5bf8e81
Christian Reyes <creyes.prog@gmail.com>
2024-10-30 13:43:44 -0400

    Add interactions story

--- src/app/components/pure-inbox-screen.stories.ts
+++ src/app/components/pure-inbox-screen.stories.ts
@@ -1,14 +1,19 @@
 
 import type { Meta, StoryObj } from '@storybook/angular';
 
-import { applicationConfig, moduleMetadata } from '@storybook/angular';
+import { importProvidersFrom } from '@angular/core';
+
+import { TasksStore } from '../state/task.store';
+
+import { moduleMetadata, applicationConfig } from '@storybook/angular';
+
+ import { fireEvent, within } from '@storybook/test';
 
 import { CommonModule } from '@angular/common';
 
 import PureInboxScreenComponent from './pure-inbox-screen.component';
 
 import { TaskModule } from './task.module';
-import { TasksStore } from '../state/task.store';
 
 const meta: Meta<PureInboxScreenComponent> = {
   component: PureInboxScreenComponent,
@@ -34,3 +39,13 @@ export const Error: Story = {
     error: true,
   },
 };
+
+ export const WithInteractions: Story = {
+   play: async ({ canvasElement }) => {
+     const canvas = within(canvasElement);
+     // Simulates pinning the first task
+     await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+     // Simulates pinning the third task
+     await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+   },
+ };
